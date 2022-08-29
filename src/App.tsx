336b32d9b0686ef7f113249507406313eb4eb59e
yamashita-ki <kirinboy96@gmail.com>
2022-08-30 00:23:30 +0900

    react-typescript
    learn how to declare types on Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import axios from "axios";
 import { useState } from "react";
 import "./App.css";
-import axios from "axios";
 import { Todo } from "./Todo";
+import { Text } from "./Text";
 import { TodoType } from "./type/TodoType";
+import { UserProfile } from "./UserProfile";
+import { UserType } from "./type/UserType";
 
 function App() {
   const [todos, setTodos] = useState<Array<TodoType>>([]);
+  const user:UserType = {
+    name:"hoge",
+    hobbies : ["映画", "ゲーム"]
+  }
   const onClickFetchData = () => {
     axios
       .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
@@ -15,6 +22,8 @@ function App() {
   };
   return (
     <div className="App">
+      <Text color="red" fontSize="18px"/>
+      <UserProfile user={user} />
       <button onClick={onClickFetchData}>データ取得</button>
       {todos.map((todo) => (
         <Todo
@@ -22,10 +31,10 @@ function App() {
           title={todo.title}
           userId={todo.userId}
           completed={todo.completed}
-          id={todo.id}
         />
       ))}
     </div>
+  
   );
 }
 
--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,7 +1,7 @@
 import { TodoType } from "./type/TodoType";
 
-export const Todo = (props:TodoType) => {
-  const { title, userId, completed,id } = props;
+export const Todo = (props: Omit<TodoType,"id">) => {
+  const { title, userId, completed } = props;
   const completeMark = completed ? "[完]" : "[未]";
   return <p>{`${completeMark} ${title}(ユーザー:${userId})`}</p>;
 };
